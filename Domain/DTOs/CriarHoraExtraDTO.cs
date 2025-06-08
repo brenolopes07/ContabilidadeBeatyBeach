42fb12a2c081647767024d0e52b818f42075d9b9
Breno Lopes <breno.skolegas07@gmail.com>
2025-06-08 18:25:57 -0300

    feat: add DTO input

---
+++ Domain/DTOs/CriarHoraExtraDTO.cs
@@ -0,0 +1,12 @@
+﻿using ContabilidadeBeatyBeach.Domain.Entity.Enum;
+
+namespace ContabilidadeBeatyBeach.Domain.DTOs
+{
+    public class CriarHoraExtraDTO
+    {
+        public DateTime Data { get; set; }
+        public decimal QuantidadeHoras { get; set; }
+        public TipoDeExtra Tipo { get; set; }
+        public int UserId { get; set; } 
+    }
+}
---
+++ Domain/DTOs/CriarOuObterUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ContabilidadeBeatyBeach.Domain.DTOs
+{
+    public class CriarOuObterUserDTO
+    {
+        public string Username { get; set; }
+        public decimal SalarioMensal { get; set; } 
+    }
+}
