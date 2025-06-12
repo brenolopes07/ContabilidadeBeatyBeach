5d8818ff8d2336892f49ac42dcdda76afb9ca7e7
Breno Lopes <breno.skolegas07@gmail.com>
2025-06-12 00:51:00 -0300

    feat: create DTO Output Delete HoraExtra

---
+++ DTOs/HoraExtra/DeletarHoraExtraDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ContabilidadeBeatyBeach.DTOs.HoraExtra
+{
+    public class DeletarHoraExtraDTO
+    {
+        public string Message { get; set; }
+        public ContabilidadeBeatyBeach.Domain.Entity.HoraExtra HoraExtraDeletada { get; set; }
+    }
+}
