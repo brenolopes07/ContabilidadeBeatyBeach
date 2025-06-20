9b285d7223928359932a35949a979c19bda06cd4
Breno Lopes <breno.skolegas07@gmail.com>
2025-06-20 17:37:01 -0300

    feat: create DTO GetComissoes

---
+++ DTOs/ComissoesDTO/GetComissoesOutputDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ContabilidadeBeatyBeach.DTOs.ComissoesDTO
+{
+    public class GetComissoesOutputDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public string Descricao { get; set; }
+
+        
+    }
+    }
