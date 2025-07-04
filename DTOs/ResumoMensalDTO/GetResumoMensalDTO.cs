99534840602d37e6fe662402bf5d0c84122c6a31
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-04 00:47:27 -0300

    feat: create DTO resumomensal data

---
+++ DTOs/ResumoMensalDTO/GetResumoMensalDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ContabilidadeBeatyBeach.DTOs.ResumoMensalDTO
+{
+    public class GetResumoMensalDTO
+    {
+        public int Id { get; set; }
+        public string mesAno { get; set; }
+
+        public decimal? TotalHoras { get; set; }
+        public decimal? TotalExtra { get; set; }
+        public decimal? TotalComissoes { get; set; }
+        public decimal SalarioTotal { get; set; }
+
+    }
+}
